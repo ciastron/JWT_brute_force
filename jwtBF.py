a345e2e33b3162a0995004bab01e9e135e04d233
ciastron <marco.brotto22@gmail.com>
2022-05-18 03:42:59 -0400

    update main program

--- jwtBF.py
+++ jwtBF.py
@@ -5,7 +5,8 @@ import base64
 import sys
 from os.path import exists
 from optparse import OptionParser
-
+from tqdm import tqdm
+import time
 
 parser = OptionParser()
 parser.add_option("-t", "--token", dest="JWT", type="string",
@@ -14,8 +15,8 @@ parser.add_option("-w", "--wordlist", dest="wordlist", type="string",
                   help="Word List file")
 (options, args) = parser.parse_args()
 
-if not (options.JWT and options.wordlist):
-    print("Insert JWT or Wordlist")
+if not (options.JWT):
+    print("Insert JWT")
     exit(0)
 
 
@@ -27,19 +28,28 @@ except Exception as e:
     print("Format of JWT wrong")
     exit(0)
 
-file_exists = exists(options.wordlist)
-if not file_exists:
-    print("File does not exist")
-    exit(0)
+if not(options.wordlist):
+    wordlist = "./wordlist"
+else:
+    file_exists = exists(options.wordlist)
+    if not file_exists:
+        print("File does not exist")
+        exit(0)
+    wordlist = options.wordlist
 
-with open(options.wordlist) as file:
+password = ""
+with open(wordlist) as file:
     lines = file.readlines()
-    for line in lines:
+    for line in tqdm(lines):
         line = line.rstrip()
         dig = hmac.new(line.encode('utf-8'), msg=msg, digestmod=hashlib.sha256).digest()
         dig64 = str(base64.b64encode(dig).decode('utf-8')).replace("=","").replace("+","-").replace("/","_")
         if dig64==digest:
-            print("Password found: " + str(line))
+            password = str(line)
             break
 
+if password == "":
+    print("Password not found")
+else:
+    print("Password found: " + password)
 
